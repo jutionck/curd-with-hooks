a929a462f63c348fb01d63d67d9ad93400584d33
jutionck <jutionck@gmail.com>
2020-07-27 16:49:07 +0700

    [REFACTOR] Cerate,Read,Delete Success

--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./user";
-
-
-const rootReducer = combineReducers({
-    userReducer: userReducer
-});
-
-export default rootReducer;
